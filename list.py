293e809875b733c71543c8eeae7d53ff4b07df56
Abbasjoyo <abbasjoyo22@gmail.com>
2026-04-23 12:42:01 +0500

    Add files via upload
    tasks

---
+++ list.py
@@ -0,0 +1,11 @@
+nums=[9,6,2,5,1,9,12]
+print("before append",nums)
+nums.append(7)
+print("after append",nums)
+
+nums.sort();
+print("after sort",nums)
+
+
+nums.reverse()
+print("after reverse",nums)
---
+++ positive.py
@@ -0,0 +1,21 @@
+# num=12
+# if num>0:
+#     print("Positive")
+
+
+# num=-0
+# if num>1:
+#     print("positive")
+# else:
+#     print("nagative")
+
+
+# num =1
+# if num%0==1:
+#     print("Even")
+# else:
+
+if num>0:
+    print("Positive")
+else:
+    print ("Nagative")
